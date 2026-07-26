7fac70ec2357ad872e2c4f7bd31529ef03e1b415
Sirius147 <lsimma7@naver.com>
2026-07-26 22:53:00 +0900

    add swea1767 lee seongil

--- 이성일/week1/SWEA1767.java
+++ 이성일/week1/SWEA1767.java
@@ -5,7 +5,13 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Sollution {
+/*
+어려웠거나 몰랐던 점:
+1.자바로 짜는 알고리즘에 익숙하지 않아 최적화 된 설계에 확신이 없다
+2. 성급하게 코드나 논리를 짜려는 경향, 확실하게 검증된 논리와 코드만 사용하기
+3. 엣지케이스 -> 엣지로직 (전체 범위를 아우르는 로직) */
+
+public class SWEA1767 {
 	
 	public static int[] ans = {0,144};
 	
@@ -40,9 +46,9 @@ public class Sollution {
 	
 	// 3. 노드인덱스 리스트 기준 DFS 진행하기
 			// 3-1. 각 노드 기준 4방향 탐색
-				// 3-2. 방향 인덱스 별로 탐색 후, 0일 경우 -1로 표기 후 다음 탐색 진행
-				// 3-3. 탐색 불가 방향일 경우, 미 탐색으로 상태 진행
-				// 3-4. 롤백 미탐색 스테이지는 함수 인자값만
+				// 3-1-2. 방향 인덱스 별로 탐색 후, 0일 경우 -1로 표기 후 다음 탐색 진행
+				// 3-1-3. 탐색 불가 방향일 경우, 미 탐색으로 상태 진행
+				// 3-1-4. 롤백, 미탐색 스테이지는 함수 인자값만
 			// 3-2. 모든 노드 탐색 완료 시 최대노드 수와 비교 후 (동일 시 전선길이 최소값으로) 탐색 종료
 	public static void dfs(int nIdx, int searchCnt, int totalWires,
 			int[][] board, int[] nodes) {
@@ -72,12 +78,12 @@ public class Sollution {
 			}
 		}
 		if (!flag) dfs(nIdx+1, searchCnt, totalWires, board, nodes);
-		// 연결가능함에도 미연결하는 탐색이 필요할 수 있다.
+		// *연결가능함에도 미연결하는 탐색이 필요할 수 있다.*
 		else dfs(nIdx+1, searchCnt, totalWires, board, nodes);
 	}
 	
 	public static void main(String[] args) throws Exception {
-		// 1. 입출력 정의 (tc 입력 받기, 출력 양식은 #tc ans
+		// 1. 입출력 정의 (tc 입력 받기, 출력 양식은 #tc ans)
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int tc = Integer.parseInt(br.readLine());
 		
@@ -116,6 +122,7 @@ public class Sollution {
 			StringBuilder sb = new StringBuilder();
 			sb.append("#").append(i).append(" ").append(ans[1]).append("\n");
 			System.out.print(sb);
+			// static 변수 초기화로 테스트케이스별로 독립적인 실행
 			ans[0] = 0;
 			ans[1] = 0;
 		}

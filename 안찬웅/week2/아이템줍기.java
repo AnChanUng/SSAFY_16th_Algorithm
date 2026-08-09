39f4662a5a1e3e38c501821d2bf64ae94ec95cb4
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-08-09 22:58:35 +0900

    20260809 안찬웅 아이템 줍기
    Implement BFS algorithm to find shortest path from character to item.

---
+++ 안찬웅/week2/아이템줍기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+/*
+    캐릭터가 아이템으로 이동하는 최단거리 구하기
+    :(characterX, characterY) -> (itemX, itemY)
+    
+    알고리즘: BFS
+    
+    1. 직사각형의 모든 점들을 찍는다.
+    2. 지나가는곳이 어딘지 어케알지
+    3. BFS로 돌리면서 최단거리 구하기
+*/
+class Solution {
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+      System.out.println("모르겠습니다..");
+      System.out.println("나중에 다시 도전하겠습니다.");
+    }
+}

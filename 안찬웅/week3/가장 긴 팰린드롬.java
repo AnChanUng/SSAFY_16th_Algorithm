26f1eba3cf4f71bf510aef90215c43f350110700
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-08-09 22:30:19 +0900

    20260809 안찬웅 가장 긴 팰린드롬
    Refactor palindrome detection logic and improve readability.

--- 안찬웅/week3/가장 긴 팰린드롬.java
+++ 안찬웅/week3/가장 긴 팰린드롬.java
@@ -9,20 +9,16 @@ import java.util.*;
     3. 처음 찾은 순간이 가장 긴 것이므로 종료
 */
 class Solution {
-    static int maxLen; // 가장 긴 팰린드롬의 길이
+    static int maxLen;
     public int solution(String s) {
         maxLen = 1;
-        palindrome(s);
-        return maxLen;
-    }
-    
-    static void palindrome(String s) {
+
         int n = s.length();
         int idx = n;
         while(idx > 1) {
             boolean flag = false;
             for(int i=0; i<n-idx+1; i++) {
-                if(isPalindrome(s, i, i+idx-1)) {
+                if(isPalindrome(s, i, i+idx-1)) { 
                     maxLen = idx;
                     flag = true;
                     break;
@@ -31,11 +27,14 @@ class Solution {
             if(flag) break;
             idx--;
         }
+
+        return maxLen;
     }
-    
     static boolean isPalindrome(String s, int left, int right) {
         while(left < right) {
-            if(s.charAt(left) != s.charAt(right)) return false;
+            if(s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
             left++;
             right--;
         }

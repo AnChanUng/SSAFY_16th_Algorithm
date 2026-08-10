73f3b38229983759e677e145894bb5681fbadcbc
LeeSeongIl <lsimma7@naver.com>
2026-08-10 17:56:21 +0900

    init: 아이템줍기.java

---
+++ 이성일/week2/아이템줍기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        // 1. 패딩으로 충돌규칙 생성
+
+        // 2. 탐색 기준 ( 4방향 중 한방향 충돌 가능 시, 회전 후 탐색)
+
+        // 3. 전방향 미충돌 시 회전 후 탐색
+
+    }
+}

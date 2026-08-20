32f4c228aca9f79f126973ae58e57099cb35f195
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-08-20 16:32:12 +0900

    20260820 안찬웅 동영상 재생기
    코드 수정

--- 안찬웅/pccp/동영상 재생기.java
+++ 안찬웅/pccp/동영상 재생기.java
@@ -19,21 +19,10 @@ import java.util.*;
 */
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        String answer = "";
-
-        // op_start도 분으로 변경
-        // op_end도 분으로 변경
         int start = translateTime(op_start);
         int end = translateTime(op_end);
         int videlTime = translateTime(video_len);
-        
-        // 1. 항시: 현재재생위치가 op_start <= 현재재생위치 <= op_end일때 오프닝이 끝나는 위치로 이동
-        //  1.1 prev: 현재위치에서 10초전으로 이동. 
-        //     남은시간이 10초미만인 경우 영상의 처음위치로 이동(0분 0초)
-        //  1.2 next: 현재위치에서 10초후로 이동. 
-        //     남은시간이 10초미만일 경우 영상의 마지막위치(동영상의 길이와 같음)로 이동
-        //  2. 명령어가 끝나고 현재시간을 00:00 형태로 출력
-        // 현재시간을 분으로 변경함
+
         int curTime = translateTime(pos);
         for(int i=0; i<commands.length; i++) {
             if(start <= curTime && curTime <= end) {
@@ -48,7 +37,7 @@ class Solution {
                 
             } else if(commands[i].equals("next")) {
                 curTime += 10;
-                if(curTime < 10) {
+                if(videlTime - curTime < 10) { // 동영상의 남은 시간이 10초 미만
                     curTime = videlTime;
                 }
             }
@@ -57,8 +46,7 @@ class Solution {
         if(start <= curTime && curTime <= end) {
             curTime = end;
         }
-        //System.out.println(curTime);
-        
+
         int t = curTime / 60;
         int m = curTime % 60;
         
